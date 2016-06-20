57af45a5199000d091d9bc485e74d2f9a8d0c1bf
Evgeny Zobnin <zobnin@gmail.com>
2016-06-20 19:45:05 +0300

    Add C.java

---
+++ samples/C.java
@@ -0,0 +1,28 @@
+package org.antrack.app;
+
+public class C {
+    public static String MAIN_DIR = "/files";
+    public static String MODULES_DIR = "/modules";
+    public static String ODEX_DIR = "/odex";
+
+    public static String SETTINGS_FILE = "/settings";
+    public static String CONTROL_FILE = "/control";
+    public static String RESULT_FILE = "/result";
+
+    public static String S_LAUNCH_WIZARD = "launchWizard"; // false
+    public static String S_USE_ADMIN = "useAdmin"; // false
+    public static String S_USE_ROOT = "useRoot"; // false
+    public static String S_UPDATE_INTERVAL = "updateInterval"; // 30
+    public static String S_PLUGIN = "plugin"; // null
+    public static String S_TOKEN = "token"; // null
+    public static String S_SHOW_HELP = "showHelp"; // true
+    public static String S_START_AT_BOOT = "startAtBoot"; // true
+    public static String S_ENABLE_SERVICE = "enableService"; // true
+    public static String S_HIDDEN = "hidden"; // false
+    public static String S_LOST = "lost"; // false
+    public static String S_SCREEN_ON_PHOTO = "screenOnPhoto"; // false
+    public static String S_BACKUP_PHONE = "backupPhone"; // null
+    public static String S_SMS_ON_SIM_CHANGE = "smsOnSimChange"; // false
+    public static String S_IMSI = "IMSI"; // null
+    public static String S_SYSTEM_APP = "systemApp"; // false
+}
